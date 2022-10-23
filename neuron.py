Fix softmax, clip weights to range [-1, 1]

neuron.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy import dot, ndarray
 
-from activation_functions import ActivationFunctionType, ActivationFunctions
+from activation_functions import ActivationFunctionType
 from error_functions import ErrorFunctionType
 
 
@@ -46,3 +46,4 @@ class Neuron:
     def update_weights(self, learning_rate: float) -> None:
         delta_weights = np.multiply(self.__input, self.delta * learning_rate)
         self.weights = np.add(self.weights, -delta_weights)
+        self.weights = np.clip(self.weights, -1, 1)