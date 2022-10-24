90d76edc4b57c2247bdcdcbef0b348fd5a23c7f8
Szymon Stasiak <szymon-stasiak@outlook.com>
2022-10-24 16:24:09 +0200

    Handle more than 2 classes in visualizer

--- data_visualizer.py
+++ data_visualizer.py
@@ -4,6 +4,8 @@ from pandas import DataFrame
 from network_enums import ProblemType
 
 class DataVisualizer:
+    __markers = ['x', 's', '^', 'd', 'X', 'o']
+
     @staticmethod
     def visualize_data(problem_type: ProblemType, data: DataFrame, result: list[float], title: str, output_file_name = None) -> None:
         if problem_type == ProblemType.CLASSIFICATION:
@@ -14,17 +16,19 @@ class DataVisualizer:
     @staticmethod
     def __visualize_classification_data(data: DataFrame, result: list[int], title: str, output_file_name = None) -> None:
         data['cls_calculated'] = result
-        first_class = data[(data['cls'] == 1)]
-        first_class_correct = first_class[(first_class['cls'] == first_class['cls_calculated'])]
-        first_class_incorrect = first_class[(first_class['cls'] != first_class['cls_calculated'])]
-        second_class = data[(data['cls'] == 2)]
-        second_class_correct = second_class[(second_class['cls'] == second_class['cls_calculated'])]
-        second_class_incorrect = second_class[(second_class['cls'] != second_class['cls_calculated'])]
+        number_of_classes = data['cls'].max()
+        correct = []
+        incorrect = []
+        for i in range(number_of_classes):
+            class_data = data[data['cls'] == i + 1]
+            correct.append(class_data[class_data['cls'] == class_data['cls_calculated']])
+            incorrect.append(class_data[class_data['cls'] != class_data['cls_calculated']])
+        axes = None
+        for i in range(number_of_classes):
+            correctness = len(correct[i]) / (len(correct[i]) + len(incorrect[i]))
+            axes = correct[i].plot.scatter(x='x', y='y', c='green', ax=axes, label=f'Correct class {i+1} ({correctness * 100:.3f}%)', marker = DataVisualizer.__markers[i % len(DataVisualizer.__markers)])
+            axes = incorrect[i].plot.scatter(x='x', y='y', c='red', ax=axes, label=f'Incorrect class {i+1} ({(1 - correctness) * 100:.3f}%)', marker = DataVisualizer.__markers[i % len(DataVisualizer.__markers)])
 
-        axes = first_class_correct.plot.scatter(x='x', y='y', c='g', label=f'Correct first class ({len(first_class_correct)/len(first_class)*100:.3f}%)', marker = 'x')
-        first_class_incorrect.plot.scatter(x='x', y='y', ax=axes, c='r', label=f'Incorrect first class ({len(first_class_incorrect)/len(first_class)*100:.3f}%)', marker = 'x')
-        second_class_correct.plot.scatter(x='x', y='y', ax=axes, c='g', label=f'Correct second class ({len(second_class_correct)/len(second_class)*100:.3f}%)', marker = 's')
-        second_class_incorrect.plot.scatter(x='x', y='y', ax=axes, c='r', label=f'Incorrect second class ({len(second_class_incorrect)/len(second_class)*100:.3f}%)', marker = 's')
         plt.title(title)
         if output_file_name is not None:
             plt.savefig(output_file_name)
